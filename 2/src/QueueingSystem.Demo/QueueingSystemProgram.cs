6265c56a1b0002c0261de933ac96821a91539134
Vadim Yaren <noonimf@gmail.com>
2021-10-18 17:58:25 +0300

    initial commit of queueing system project

---
+++ 2/src/QueueingSystem.Demo/QueueingSystemProgram.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QueueingSystem.Demo
+{
+    class QueueingSystemProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Queueing System!");
+        }
+    }
+}
