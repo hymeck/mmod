d28806b11d6b5488a8c7a88254ee7110330c56b3
Vadim Yaren <noonimf@gmail.com>
2021-10-26 20:44:57 +0300

    added initial characteristics of queueing system

---
+++ 2/src/QueueingSystem.Demo/Utils/ArgParser.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QueueingSystem.Demo.Utils
+{
+    public class ArgParser
+    {
+        private const double DefaultArrivalRate = 1d;
+        private const double DefaultServiceRate = 2d;
+        private const int DefaultServerCount = 1;
+        private const int DefaultQueueCapacity = 1;
+        private const double DefaultWaitingTime = double.PositiveInfinity;
+        
+        private readonly IReadOnlyList<string> _args;
+        private double? _arrivalRate;
+        private double? _serviceRate;
+        private int? _serverCount;
+        private int? _queueCapacity;
+        private double? _waitingTime;
+
+        public ArgParser(IReadOnlyList<string> args)
+        {
+            _args = args ?? Array.Empty<string>();
+        }
+
+        public double GetArrivalRate() => _arrivalRate ??= _args.Count == 1
+            ? double.TryParse(_args[0], out var arrivalRate)
+                ? arrivalRate
+                : DefaultArrivalRate
+            : DefaultArrivalRate;
+
+        public double GetServerCount() => _serviceRate ??= _args.Count == 2
+            ? double.TryParse(_args[1], out var serviceRate)
+                ? serviceRate
+                : DefaultServiceRate
+            : DefaultServiceRate;
+        
+        public int GetServiceRate() => _serverCount ??= _args.Count == 3
+            ? int.TryParse(_args[2], out var serviceRate)
+                ? serviceRate
+                : DefaultServerCount
+            : DefaultServerCount;
+        
+        public int GetQueueCapacity() => _queueCapacity ??= _args.Count == 4
+            ? int.TryParse(_args[3], out var queueCapacity)
+                ? queueCapacity
+                : DefaultQueueCapacity
+            : DefaultQueueCapacity;
+        
+        public double GetWaitingTime() => _waitingTime ??= _args.Count == 5
+            ? double.TryParse(_args[4], out var waitingTime)
+                ? waitingTime
+                : DefaultWaitingTime
+            : DefaultWaitingTime;
+    }
+}
---
+++ 2/src/QueueingSystem/QueueingSystemCharacteristics.cs
@@ -0,0 +1,44 @@
+﻿using Ardalis.GuardClauses;
+
+namespace QueueingSystem
+{
+    /// <summary>
+    /// Contains values describing queueing system. 
+    /// </summary>
+    public class QueueingSystemCharacteristics
+    {
+        public QueueingSystemCharacteristics(double arrivalRate, double serviceRate, int serverCount, int queueCapacity, double waitingTime)
+        {
+            ArrivalRate = Guard.Against.Negative(arrivalRate, nameof(arrivalRate));
+            ServiceRate = Guard.Against.Negative(serviceRate, nameof(serviceRate));
+            ServerCount = Guard.Against.NegativeOrZero(serverCount, nameof(serverCount));
+            QueueCapacity = Guard.Against.NegativeOrZero(queueCapacity, nameof(queueCapacity));
+            WaitingTime = Guard.Against.Negative(waitingTime, nameof(waitingTime));
+        }
+
+        /// <summary>
+        /// Mean rate of arrival.
+        /// </summary>
+        public double ArrivalRate { get; }
+        
+        /// <summary>
+        /// Mean service rate.
+        /// </summary>
+        public double ServiceRate { get; }
+        
+        /// <summary>
+        /// Number of servers in a system.
+        /// </summary>
+        public int ServerCount { get; }
+        
+        /// <summary>
+        /// Capacity of a queue.
+        /// </summary>
+        public int QueueCapacity { get; }
+        
+        /// <summary>
+        /// Mean of waiting time.
+        /// </summary>
+        public double WaitingTime { get; }
+    }
+}
